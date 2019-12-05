ef9179ac6f4f70e3edfbc9786138b45ba9f4a49d
Acinate <acinate@hotmail.com>
2019-12-05 15:33:34 -0500

    alpha_file now saves data to csv file

--- alpha/alpha_file.py
+++ alpha/alpha_file.py
@@ -23,8 +23,34 @@ class File:
         fp.write(json_dictionary)
         print("Wrote results to file: " + self.filename)
 
-    def read_datapoints_from_file(self, symbol):
-        self.filename = self.directory + symbol + "_datapoints.json"
-        fp = open(self.filename, "r")
-        datapoints = json.load(fp)
-        return datapoints
+    def write_datapoints_to_csv(self, datapoints, symbol):
+        self.filename = self.directory + symbol + "_datapoints.csv"
+        lines = []
+        for key in datapoints.keys():
+            datapoint = datapoints[key]
+            if datapoint.sma is not None \
+                    and datapoint.ema is not None \
+                    and datapoint.vwap is not None \
+                    and datapoint.macd is not None \
+                    and datapoint.rsi is not None \
+                    and datapoint.adx is not None:
+                line = key + "," + \
+                       datapoint.close + "," + \
+                       datapoint.sma + "," + \
+                       datapoint.ema + "," + \
+                       datapoint.vwap + "," + \
+                       datapoint.macd + "," + \
+                       datapoint.rsi + "," + \
+                       datapoint.adx
+                lines.append(line)
+        fp = open(self.filename, "w")
+        for line in lines:
+            fp.write(line + "\n")
+        print("Wrote results to file: " + self.filename)
+
+
+def read_datapoints_from_file(self, symbol):
+    self.filename = self.directory + symbol + "_datapoints.json"
+    fp = open(self.filename, "r")
+    datapoints = json.load(fp)
+    return datapoints
