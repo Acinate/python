6525a1f50d8feb3de06cf209590a59d5d63597c4
Acinate <acinate@hotmail.com>
2019-12-05 14:25:03 -0500

    alpha_file.py now writes to a directory: data

---
+++ alpha/alpha_file.py
@@ -0,0 +1,30 @@
+import json
+
+
+class File:
+    def __init__(self):
+        self.filename = "unknown_datapoints.json"
+        self.directory = "data/"
+
+    def write_datapoints_to_file(self, datapoints, symbol):
+        self.filename = self.directory + symbol + "_datapoints.json"
+        good_datapoints = {}
+        for key in datapoints.keys():
+            datapoint = datapoints[key]
+            if datapoint.sma is not None \
+                    and datapoint.ema is not None \
+                    and datapoint.vwap is not None \
+                    and datapoint.macd is not None \
+                    and datapoint.rsi is not None \
+                    and datapoint.adx is not None:
+                good_datapoints[key] = datapoint
+        fp = open(self.filename, "w")
+        json_dictionary = json.dumps({k: v.__dict__ for k, v in good_datapoints.items()}, sort_keys=True, indent=4)
+        fp.write(json_dictionary)
+        print("Wrote results to file: " + self.filename)
+
+    def read_datapoints_from_file(self, symbol):
+        self.filename = self.directory + symbol + "_datapoints.json"
+        fp = open(self.filename, "r")
+        datapoints = json.load(fp)
+        return datapoints
