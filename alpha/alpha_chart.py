f5fcc6ca6e42f05d870c9036c1c81846c624db09
Acinate <acinate@hotmail.com>
2019-12-05 17:40:04 -0500

    Revert "fixed some bugs, chart was displaying backwards"
    This reverts commit 63721b0b

--- alpha/alpha_chart.py
+++ alpha/alpha_chart.py
@@ -9,22 +9,13 @@ class AlphaChart:
         self.load_chart_data()
 
     def load_chart_data(self):
-        df = pd.read_csv("data/" + self.symbol + "_datapoints.csv",
-                         names=['Date', 'Close', 'SMA', 'EMA', 'VWAP', 'MACD', 'RSI', 'ADX'])
-        self.df = df
-        self.df['Date'] = df['Date'].values[::-1]
-        self.df['Close'] = df['Close'].values[::-1]
-        self.df['SMA'] = df['SMA'].values[::-1]
-        self.df['EMA'] = df['EMA'].values[::-1]
-        self.df['VWAP'] = df['VWAP'].values[::-1]
-        self.df['MACD'] = df['MACD'].values[::-1]
-        self.df['RSI'] = df['RSI'].values[::-1]
-        self.df['ADX'] = df['ADX'].values[::-1]
+        self.df = pd.read_csv("data/" + self.symbol + "_datapoints.csv",
+                              names=['Date', 'Close', 'SMA', 'EMA', 'VWAP', 'MACD', 'RSI', 'ADX'])
 
     def display_chart(self):
         # Plot Price Chart with EMA, SMA and VWAP
         ax = plt.subplot(211)
-        plt.plot(self.df['Close'], label=self.symbol, color='black')
+        plt.plot(self.df['Close'], label=self.symbol, color='blue')
         plt.plot(self.df['SMA'], label='SMA', color='green')
         plt.plot(self.df['EMA'], label='EMA', color='cyan')
         plt.plot(self.df['VWAP'], label='VWAP', color='purple')
