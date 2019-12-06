3450b47110e42f17272cf31dcae4f0bf0762bae4
Acinate <acinate@hotmail.com>
2019-12-06 10:36:26 -0500

    added tensorflow image classification example

---
+++ tensorflow/image_classification.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Specify the layers
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # Input layer
+    keras.layers.Dense(128, activation="relu"),  # Hidden layer
+    keras.layers.Dense(10, activation="softmax")  # Output layer
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+# Train the data
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested accuracy: ", test_acc)
+
+prediction = model.predict(test_images)
+
+print(class_names[np.argmax(prediction[0])])
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
