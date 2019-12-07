580f9379e10fa8b6591cf80b0a4fdfb053ea0be8
Acinate <acinate@hotmail.com>
2019-12-07 12:04:31 -0500

    added model trainer

---
+++ q_trader/train.py
@@ -0,0 +1,49 @@
+from alpha.alpha_file import AlphaFile
+from q_trader.agent import Agent
+from q_trader.functions import *
+
+symbol = "JNUG"
+window_size = 10
+epoch_count = 1000
+
+agent = Agent(window_size)
+data = AlphaFile(symbol).read_datapoints_from_csv()
+l = len(data) - 1
+batch_size = 32
+
+for e in range(epoch_count + 1):
+    print("Episode ", e, "/", epoch_count)
+    state = getState(data, 0, window_size + 1)
+
+    total_profit = 0
+    agent.inventory = []
+
+    for t in range(l):
+        action = agent.act(state)
+
+        # hold
+        next_state = getState(data, t + 1, window_size + 1)
+        reward = 0
+
+        if action == 1:  # buy
+            agent.inventory.append(data[t])
+            print("Buy: ", formatPrice(data[t]))
+        elif action == 2 and len(agent.inventory) > 0:  # sell
+            bought_price = agent.inventory.pop(0)
+            reward = max(data[t] - bought_price, 0)
+            total_profit += data[t] - bought_price
+            print("Sell: ", formatPrice(data[t]), " | Profit: " + formatPrice(data[t] - bought_price))
+        done = True if t == l - 1 else False
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
+
+        if done:
+            print("--------------------------------")
+            print("Total Profit: " + formatPrice(total_profit))
+            print("--------------------------------")
+
+        if len(agent.memory) > batch_size:
+            agent.expReplay(batch_size)
+
+    if e % 10 == 0:
+        agent.model.save("models/model_ep" + str(e))
