3fc5d8171432f4acafa87b7d20bed7c96bf79d64
Acinate <acinate@hotmail.com>
2019-12-13 13:09:07 -0500

    cleaned up train.py

--- q_trader/train.py
+++ q_trader/train.py
@@ -2,13 +2,13 @@ from alpha.alpha_file import AlphaFile
 from q_trader.agent import Agent
 from q_trader.functions import *
 
-symbol = "JNUG"
+stock_name = "MSFT"
 window_size = 10
 epoch_count = 1000
 
 agent = Agent(window_size, action_size=3)
-data = format_alpha_data(AlphaFile(symbol).read_datapoints_from_csv())
-l = len(data) - 1
+data = format_alpha_data(AlphaFile(stock_name).read_datapoints_from_csv())
+data_size = len(data) - 1
 batch_size = 32
 
 for e in range(epoch_count + 1):
@@ -18,34 +18,41 @@ for e in range(epoch_count + 1):
     total_profit = 0
     agent.inventory = []
 
-    for t in range(l):
+    for t in range(data_size):
+        # Get best action based on policy
         action = agent.act(state)
-
-        # hold
+        # Get the previous 10 days of price history
         next_state = get_state(data, t + 1, window_size + 1)
+        # Initialize our reward as zero
         reward = 0
-
-        if action == 1:  # buy
+        # Action 1 = BUY
+        if action == 1 and len(agent.inventory) == 0:
             agent.inventory.append(data[t][0])
-            # print("Buy: ", formatPrice(data[t][0]))
-        elif action == 2 and len(agent.inventory) > 0:  # sell
+        # Action 2 = SELL
+        elif action == 2 and len(agent.inventory) > 0:
             bought_price = agent.inventory.pop(0)
-            # reward = max(data[t][0] - bought_price, 0)
             reward = data[t][0] - bought_price
             total_profit += data[t][0] - bought_price
-            # print("Sell: ", formatPrice(data[t][0]), " | Profit: " + formatPrice(data[t][0] - bought_price))
-        done = True if t == l - 1 else False
-        agent.memory.append((state, action, reward, next_state, done))
-        state = next_state
-
+        # Check if we are at the last datapoint
+        done = True if t == data_size - 1 else False
         if done:
+            # Sell Remaining Shares
+            while len(agent.inventory) > 0:
+                bought_price = agent.inventory.pop(0)
+                reward = data[t][0] - bought_price
+                total_profit += data[t][0] - bought_price
             print("--------------------------------")
             print("Total Profit: " + formatPrice(total_profit))
             print("--------------------------------")
-
+        # Add the current frame to memory
+        agent.memory.append((state, action, reward, next_state, done))
+        # Transition to the next state
+        state = next_state
+        # If memory is getting full, train the actor and critic nn
+        # Calculate Q-Values, Update Q-Table, and clear memory
         if len(agent.memory) > batch_size:
             agent.exp_replay(batch_size)
 
     if e % 10 == 0:
-        agent.actor.save("models/" + symbol + "_model_ep" + str(e) + ".hdf5")
-        agent.actor.save("models/" + symbol + "_model_ep" + str(e) + ".hdf5")
+        agent.actor.save("models/" + stock_name + "_model_ep" + str(e) + ".hdf5")
+        agent.actor.save("models/" + stock_name + "_model_ep" + str(e) + ".hdf5")
