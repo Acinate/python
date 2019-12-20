ecf199285ce1b4ca9f07130575fa274d845ae2aa
Acinate <acinate@hotmail.com>
2019-12-19 22:30:30 -0500

    added new data, updated prediction script

--- stock_prediction/stock_prediction.py
+++ stock_prediction/stock_prediction.py
@@ -1,66 +1,91 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 
-from alpha.alpha_api import AlphaApi
+# Importing the training set
+dataset_train = pd.read_csv('data/VZ_Stock_Price_Train.csv')
+# Reverse rows (Data is stored in descending date, we need ascending)
+dataset_train = dataset_train.reindex(index=dataset_train.index[::-1])
+training_set = dataset_train.iloc[:, 1:2].values
 
-# Stock Symbol
-symbol = "MSFT"
-
-# Import the training data
-# Gets OHLCV Daily Values for Symbol
-api = AlphaApi(symbol)
-train_data = api.get_daily(symbol)
-
-# Scale the data
+# Scale the prices from 0 - 1
 sc = MinMaxScaler(feature_range=(0, 1))
-training_set_scaled = sc.fit_transform(train_data)
+training_set_scaled = sc.fit_transform(training_set)
 
-# Create a data structure with 60 timesteps and 1 output
-x_train = []
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
 y_train = []
-for i in range(10, len(train_data)):
-    x_train.append(training_set_scaled[i - 10:i, 0])
-    y_train.append(training_set_scaled[i, 4])
-x_train = np.array(x_train)
-y_train = np.array(y_train)
+for i in range(60, dataset_train.shape[0]):
+    X_train.append(training_set_scaled[i - 60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
 
-# Reshape the data
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-# Build the NN
+# Part 2 - Building the RNN
+
+# Initialising the RNN
 regressor = Sequential()
 
-# Add the first LTSM layer and some Dropout regularisation
-regressor.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+# Adding the first LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
 
-# Add the second LSTM layer and some Dropout regularisation
+# Adding a second LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
 
-# Add a third LSTM layer and some Dropout regularisation
+# Adding a third LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
 
-# Add a fourth LSTM layer and some Dropout regularisation
+# Adding a fourth LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units=50))
 regressor.add(Dropout(0.2))
 
-# Add the output layer
+# Adding the output layer
 regressor.add(Dense(units=1))
 
-# Compile the RNN
+# Compiling the RNN
 regressor.compile(optimizer='adam', loss='mean_squared_error')
 
 # Fitting the RNN to the Training set
-regressor.fit(x_train, y_train, epochs=100, batch_size=32)
+regressor.fit(X_train, y_train, epochs=1, batch_size=32)
 
-# Plot data
-ax = plt.subplot(211)
-plt.plot(train_data[:, 0], train_data[:, 4])
+# Part 3 - Making the predictions and visualising the results
+
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('data/VZ_Stock_Price_Test.csv')
+dataset_test = dataset_test.reindex(index=dataset_test.index[::-1])
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+test = dataset_test['open']
+print(test)
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train['open'], dataset_test['open']), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i - 60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color='red', label='Real Google Stock Price')
+plt.plot(predicted_stock_price, color='blue', label='Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
 plt.show()
