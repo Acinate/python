3e6415233cca759678c5d151552cdbf8b2335ec1
Acinate <acinate@hotmail.com>
2019-12-22 23:16:15 -0500

    fixed error in 136_single_number

--- leetcode/136_single_number/a.py
+++ leetcode/136_single_number/a.py
@@ -1,20 +1,14 @@
 from typing import List, Mapping
 
-test_nums = [1, 1, 2, 2, 3, 3, 4, 5, 5]
-
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         num_map: Mapping[int, bool] = {}
         for num in nums:
-            if num_map[num] is None:
+            if num_map.get(num) is None:
                 num_map[num] = False
             elif not num_map[num]:
                 num_map[num] = True
         for num in nums:
             if not num_map[num]:
                 return num
-
-
-ans = Solution().singleNumber(test_nums)
-print(ans)
