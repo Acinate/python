b63ed06898458c2dfe3b7f48bafb407bb03f4f16
Jason <acinate@hotmail.com>
2020-01-28 22:17:25 -0500

    min stack

---
+++ leetcode/155_min_stack.py
@@ -0,0 +1,27 @@
+# Today was a day of poor time management
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+    def push(self, x: int) -> None:
+        assert NotImplemented
+
+    def pop(self) -> None:
+        assert NotImplemented
+
+    def top(self) -> int:
+        assert NotImplemented
+
+    def getMin(self) -> int:
+        assert NotImplemented
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
