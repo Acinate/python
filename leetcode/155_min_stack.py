ba95040c5653fa307fdd66fb1729987da24d0ce6
Jason <acinate@hotmail.com>
2020-02-08 22:17:51 -0500

    added problem number to filename

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -1,27 +1,40 @@
-# Today was a day of poor time management
+from typing import List
+from unittest import TestCase
+
 
 class MinStack:
+    stack: List
 
     def __init__(self):
         """
         initialize your data structure here.
         """
+        self.stack = []
+        self.min = None
 
     def push(self, x: int) -> None:
-        assert NotImplemented
+        cur_min = self.getMin()
+        if cur_min is None or cur_min > x:
+            cur_min = x
+        self.stack.append((x, cur_min))
 
     def pop(self) -> None:
-        assert NotImplemented
+        return self.stack.pop(-1)
 
     def top(self) -> int:
-        assert NotImplemented
+        return self.stack[-1][0] if len(self.stack) > 0 else None
 
     def getMin(self) -> int:
-        assert NotImplemented
+        return self.stack[-1][1] if len(self.stack) > 0 else None
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+
+class TestSolution(TestCase):
+    def test(self):
+        min_stack: MinStack = MinStack()
+        min_stack.push(-2)
+        min_stack.push(0)
+        min_stack.push(-3)
+        self.assertEqual(min_stack.getMin(), -3)
+        min_stack.pop()
+        self.assertEqual(min_stack.top(), 0)
+        self.assertEqual(min_stack.getMin(), -2)
